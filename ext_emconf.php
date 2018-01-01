ae96173d7e9eb729069aa6d94365b32cc871b24e
Andreas Stieger <stieger@e-conomix.at>
2018-01-01 14:03:18 +0100

    :memo: update version constraints for testing to TYPO3 9.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,8 +23,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'php' => '7.0.0-7.1.99',
-      'typo3' => '7.6.13-8.7.99',
+       'typo3' => '9.0.0',
     ),
     'conflicts' => 
     array (
